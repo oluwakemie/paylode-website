c13f136a81cb7deb03dd0e794e96b02d08f8ec41
Ogundele Caleb <ogundelecaleb13@gmail.com>
2023-04-25 10:28:59 +0100

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,26 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react'
+import Footer from './component/footer'
+import Header from './component/header'
 
-function App() {
+const App = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+        <Header/>
+        <div className='h-[400px]'>
+<div>
+    <section>
+        <div>
+            <h2>ADVANCED PAYMENT GATEWAY SOLUTION FOR AFRICA</h2>
+        </div>
+    <div>
+        <img src="" alt="" />
     </div>
-  );
+    </section>
+</div>
+        </div>
+        <Footer/>
+    </div>
+  )
 }
 
-export default App;
+export default App
